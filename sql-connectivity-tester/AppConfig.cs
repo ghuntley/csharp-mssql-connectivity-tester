328857741b5afcd4452fb33755f07694e5659e05
Geoffrey Huntley <ghuntley@ghuntley.com>
2013-07-09 15:21:25 +1000

    Initial commit

---
+++ sql-connectivity-tester/AppConfig.cs
@@ -0,0 +1,9 @@
+﻿using System.Configuration;
+
+namespace sql_connectivity_tester
+{
+    public static class AppConfig
+    {
+        public static string ConnectionString = ConfigurationManager.ConnectionStrings["DatabaseConnectionString"].ConnectionString;
+    }
+}
---
+++ sql-connectivity-tester/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace sql_connectivity_tester
+{
+    class Program
+    {
+        public enum ExitCode : int
+        {
+            Success = 0,
+            UnknownError = 1
+        }
+
+        static int Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Connecting to: {0}", AppConfig.ConnectionString);
+                using (var connection = new SqlConnection(AppConfig.ConnectionString))
+                {
+                    var query = "select 1";
+                    Console.WriteLine("Executing: {0}", query);
+
+                    var command = new SqlCommand(query, connection);
+
+                    connection.Open();
+                    Console.WriteLine("SQL Connection successful.");
+
+                    command.ExecuteScalar();
+                    Console.WriteLine("SQL Query execution successful.");
+
+                    return (int)ExitCode.Success;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Failure: {0}", ex.Message);
+                return (int)ExitCode.UnknownError;
+            }
+        }
+    }
+}
